4d1757a9d6b9f42b0d7e27b04eab93e669dd8fef
Cyber Dolphin <lol.maxpower@yandex.ru>
2022-06-27 11:45:43 +0300

    Update smoke test example

--- smoke_test.sh
+++ smoke_test.sh
@@ -1,3 +1,11 @@
+# Smoke test for localhost
+
 echo ruok | nc 127.0.0.1 2181
+
+# Smoke test for external ip
+
+echo ruok | nc 192.168.56.210 2181
+
+# !!! Need open port on ufw !!!
 
 # if imok this ok, else server not work
